22a7564b1fc7d97731e5ae6263336850c70bc142
rodrigo.a.lopes <rodrigo.a.lopes@itamaraty.gov.br>
2018-06-06 15:23:30 -0400

    component home ok

--- landing/app/src/index.js
+++ landing/app/src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import {Route} from 'react-router'
+import { ConnectedRouter } from 'react-router-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import Home from './containers/Home'
+import cfg from './store';
+
+const store = cfg.configStore()
+
+ReactDOM.render(
+    <main>
+        <Provider store= {store}>
+            <ConnectedRouter history= {cfg.history}>
+                <Route exact path="/" component={Home} />
+            </ConnectedRouter>
+        </Provider>
+    </main>,
+    document.getElementById('root')
+)
