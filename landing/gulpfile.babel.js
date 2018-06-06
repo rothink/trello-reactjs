88b25a98c22b56a007625d41e2939c5d41625aa2
rodrigo.a.lopes <rodrigo.a.lopes@itamaraty.gov.br>
2018-06-06 12:49:45 -0400

    front end ok

--- landing/gulpfile.babel.js
+++ landing/gulpfile.babel.js
@@ -8,8 +8,8 @@ import util from 'gulp-util'
 import concat from 'gulp-concat'
 import plumber from 'gulp-plumber'
 import imagemin from 'gulp-imagemin'
-import minifyHtml from 'gulp-minify-html'
 import minifyCss from 'gulp-minify-css'
+import minifyHtml from 'gulp-minify-html'
 import rev from 'gulp-rev'
 import revCollector from 'gulp-rev-collector'
 import uglify from 'gulp-uglify'
@@ -39,66 +39,62 @@ const igniteServer = () => {
     })
 }
 
+
 gulp.task('b-html', () => {
-    return 
-        gulp
-            .src(PATH.htmlSrc.concat('**/*.html'))
-            .pipe(gulp.dest(PATH.buildDir.concat('/')))
-            .pipa(livereload())
+	return gulp
+			.src(PATH.htmlSrc.concat('**/*.html'))
+			.pipe(gulp.dest(PATH.buildDir.concat('/')))
+			.pipe(livereload())
 })
 
 gulp.task('b-css', () => {
-    return 
-        gulp
-            .src(PATH.sassSrc.concat('**/*.css'))
-            .pipe(sass({
-                includePaths: require('node-neat').includePaths,
-                style: 'nested',
-                onError: () => {
-                    console.info('error sass!');
-                }
-            }))
-            .pipe(plumber({errorHandler: onError}))
-            .pipe(gulp.dest(PATH.buildDir.concat('/css')))
-            .pipe(livereload())
+	return gulp
+			.src(PATH.sassSrc.concat('**/*.scss'))
+			.pipe(sass({
+				includePaths: require('node-neat').includePaths,
+				style: 'nested',
+				onError: () => {
+					console.log('Opps!! Something happens when compile SASS')
+				}
+			}))
+			.pipe(plumber({ errorHandler: onError }))
+			.pipe(gulp.dest(PATH.buildDir.concat('/css')))
+			.pipe(livereload())
 })
 
 gulp.task('b-js', () => {
-    return 
-        gulp
-            .src(PATH.jsSrc.concat('**/*.js'))
-            .pipe(babel({
-                presets: ['es2015']
-            }))
-            .pipe(plumber({errorHandler: onError}))
-            .pipe(changed(PATH.buildDir.concat('/js')))
-            .pipe(gulp.dest(PATH.buildDir.concat('/js')))
-            .pipe(livereload())
+	return gulp
+			.src(PATH.jsSrc.concat('*.js'))
+			.pipe(babel({
+				presets: ['es2015']
+			}))
+			.pipe(plumber({ errorHandler : onError }))
+			.pipe(changed(PATH.buildDir.concat('/js')))
+			.pipe(gulp.dest(PATH.buildDir.concat('/js')))
+			.pipe(livereload())
 })
 
 gulp.task('b-img', () => {
-    return 
-        gulp
-            .src(PATH.imgSrc.concat('**/*.+(png|jpg|jpeg|gif|svg'))
-            .pipe(changed(PATH.buildDir.concat('/imgages')))
-            .pipe(gulp.dest(PATH.buildDir.concat('/imgages')))
-            .pipe(livereload())
+	return gulp
+			.src(PATH.imgSrc.concat('**/*.+(png|jpg|jpeg|gif|svg)'))
+			.pipe(changed(PATH.buildDir.concat('/images')))
+			.pipe(gulp.dest(PATH.buildDir.concat('/images')))
+			.pipe(livereload())
 })
 
 gulp.task('watch', () => {
-    gulp.watch('src/*.html', ['b-html'])
-    gulp.watch('src/styles/*.css', ['b-css'])
-    gulp.watch(PATH.jsSrc.concat('**/*.js'), ['b-js'])
-    gulp.watch(PATH.imgSrc.concat('**/*.+(png|jpg|jpeg|gif|svg'), ['b-img'])
-    
+	gulp.watch('src/*.html', ['b-html'])
+	gulp.watch('src/styles/**', ['b-css'])
+	gulp.watch(PATH.jsSrc.concat('**/*.js'), ['b-js'])
+	gulp.watch(PATH.imgSrc.concat('**/*.+(png|jpg|jpeg|gif|svg)'), ['b-img'])
 })
 
 gulp.task('build', ['b-html', 'b-css', 'b-js', 'b-img'], () => {
-    return igniteServer()
+	return igniteServer()
 })
 
 const ENV = process.env.SERVER_ENV || 'development'
 
-if(ENV === 'development') {
-    gulp.task('default', ['build', 'watch'])
+if (ENV === 'development') {
+	gulp.task('default', ['build', 'watch'])
 }
